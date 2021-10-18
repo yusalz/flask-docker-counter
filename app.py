4ccdc97a946177f94fd6da168c40829060401567
yusalz <salimz.yu@gmail.com>
2021-10-18 07:31:09 +0300

    Implement about page

--- app.py
+++ app.py
@@ -15,6 +15,10 @@ def stat():
     count = redis.incr('hits')
     return 'Counter value: {} (Value incremented)\n'.format(int(count))
 
+@app.route('/about')
+def about():
+    return '<h3>Hello from Yuliana</h3>' 
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
 
