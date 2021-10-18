d43da80ff618ec23291778bf52ec92f749550c0b
yusalz <salimz.yu@gmail.com>
2021-10-18 06:44:03 +0300

    Implement index page with counter value

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from redis import Redis
+
+app = Flask(__name__)
+redis = Redis(host='redis', port=6379)
+
+@app.route('/')
+def index():
+    hits_value = redis.get('hits')
+    count = 0 if hits_value is None else hits_value
+    return 'Counter value: {}\n'.format(count)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=True)
+
