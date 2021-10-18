f2ce6d7307e88435d817205d2addc288d38fe36d
yusalz <salimz.yu@gmail.com>
2021-10-18 07:19:45 +0300

    Implement stat page with counter value fnd increment logic

--- app.py
+++ app.py
@@ -8,7 +8,12 @@ redis = Redis(host='redis', port=6379)
 def index():
     hits_value = redis.get('hits')
     count = 0 if hits_value is None else hits_value
-    return 'Counter value: {}\n'.format(count)
+    return 'Counter value: {}\n'.format(int(count))
+
+@app.route('/stat')
+def stat():
+    count = redis.incr('hits')
+    return 'Counter value: {} (Value incremented)\n'.format(int(count))
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
